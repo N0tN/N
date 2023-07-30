0504112bff183b6e07cac8922fd266626f2dc5d9
N0tN <99139682+N0tN@users.noreply.github.com>
2023-07-30 17:58:00 +0200

    Create install-onlyHL.sh

---
+++ scripts/hl/install-onlyHL.sh
@@ -0,0 +1,105 @@
+#!/bin/sh
+
+set -e
+
+rm -rf ~/.tmp
+mkdir ~/.tmp
+cd ~/.tmp
+
+
+if [ -f /usr/bin/apt ];then
+	sudo apt install -y python libsdl2-2.0-0 libsdl2-dev gcc g++ libfreetype6-dev pkg-config git unzip fontconfig libfontconfig1-dev
+elif [ -f /usr/bin/pacman ];then
+	sudo pacman -S --needed sdl2 gcc fontconfig python freetype2 git unzip pkgconf
+fi
+
+if [ $(uname -m) == "x86_64" ]; then
+  ARGS="--64bits"
+fi
+
+
+
+
+
+git clone --recursive --depth=1 https://github.com/FWGS/hlsdk-xash3d hl
+
+cd hl
+
+./waf configure --enable-lto --enable-poly-opt --build-type=release $ARGS
+./waf build
+mkdir res
+./waf install --strip --destdir=res
+mv res ../hlsdk
+
+cd ..
+
+
+git clone --recursive --depth=1 https://github.com/FWGS/xash3d-fwgs
+cd xash3d-fwgs
+
+./waf configure --build-type=release --enable-lto --enable-poly-opt $ARGS
+./waf build
+./waf install --strip --destdir=res
+
+mv res ../engine
+
+cd ..
+
+rm -rf hl xash3d-fwgs ~/.xash3d
+
+mkdir ~/.xash3d
+mv * ~/.xash3d
+mv ~/.xash3d/hlsdk/valve ~/.xash3d/valve
+rm -rf ~/.xash3d/hlsdk
+mv ~/.xash3d/engine ~/.games/hl
+
+cd
+rm -rf ~/.tmp
+
+
+
+echo '#!/bin/sh
+export LD_LIBRARY_PATH=/home/$USER/.games/hl:$LD_LIBRARY_PATH
+export XASH3D_BASEDIR="$HOME/.xash3d"
+/home/$USER/.games/hl/xash3d "$@"' > hl
+
+chmod +x hl
+sudo mv hl /bin/hl
+
+
+
+
+cd ~/.games/hl
+
+wget https://raw.githubusercontent.com/IsaacMvmv/N/main/icons/hl.png
+
+cd ~/.xash3d
+
+Lang=$(echo $LANG | grep es)
+
+if [ $Lang = "" ];then
+	wget https://github.com/N0tN/N0tN/releases/download/webpage/V1.zip
+	unzip V1.zip
+	rm V1.zip
+else
+	wget https://github.com/N0tN/N0tN/releases/download/webpage/V2.zip
+	unzip V2.zip
+	rm V2.zip
+fi
+
+mv ~/.games/hl/valve/* valve/.
+
+mkdir -pv ~/.local/share/applications/
+
+echo "[Desktop Entry]
+Type=Application
+Version=1.0
+Name=Half life
+Comment=Xash3d engine and hlsdk .so files
+Exec=/bin/hl
+Icon=/home/$USER/.games/hl/hl.png
+Terminal=false
+Categories=Game;" > ~/.local/share/applications/HalfLife.desktop
+chmod +x ~/.local/share/applications/HalfLife.desktop
+
+echo "Launch game by typing hl or use app icon"
